b8ac47050c8d3b527e9c50582f4e6010806888de
giovannafabivaz <gfvaz@sga.pucminas.br>
2025-12-05 18:20:22 -0300

    fix: trying to fix server

--- src/main/java/com/pucintegra/api/config/WebConfig.java
+++ src/main/java/com/pucintegra/api/config/WebConfig.java
@@ -2,31 +2,28 @@ package com.pucintegra.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry; 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.lang.NonNull;
 
+import java.nio.file.Paths; 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(@NonNull ViewControllerRegistry registry) {
-        // Redireciona a raiz "/" para o arquivo index.html
-        registry.addViewController("/").setViewName("forward:/index.html");
-    }
-    @Override
-    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
 
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("classpath:/static/uploads/");
-    }
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        String uploadPath = Paths.get("src/main/resources/static/uploads").toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadPath + "/");
     }
 }
