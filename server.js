74cd0b6588c4701df5f0afc4078cc244a85dec5e
teus1st <164429561+teus1st@users.noreply.github.com>
2025-12-05 11:06:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -339,5 +339,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(Servidor rodando em http://localhost:${PORT});
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
